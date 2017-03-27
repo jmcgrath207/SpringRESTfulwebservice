d93eb511d4ee6eff53bde36f54794eb4c1e2cef4
john <sonance207@gmail.com>
2017-03-27 17:49:03 -0500

    working on fixing java issue

--- src/main/java/com/johnmcgrath/GreetingController.java
+++ src/main/java/com/johnmcgrath/GreetingController.java
@@ -7,6 +7,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RestController
 public class GreetingController {
@@ -14,7 +16,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting" , method = RequestMethod.POST)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
